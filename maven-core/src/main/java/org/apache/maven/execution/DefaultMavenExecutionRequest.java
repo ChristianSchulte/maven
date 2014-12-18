68e63625eba655a2f76a18a25d48a7671c9402cb
Christian Schulte <cs@schulte.it>
2014-12-18 02:37:09 +0100

    [MNG-5738] Addition of command line flag '--legacy-reactor-resolution'.
    o Updated to add command line option '--legacy-reactor-resolution' to allow
      disabling workspace resolution introduced in Maven 3 as discussed at
      MPIR-238.

--- maven-core/src/main/java/org/apache/maven/execution/DefaultMavenExecutionRequest.java
+++ maven-core/src/main/java/org/apache/maven/execution/DefaultMavenExecutionRequest.java
@@ -165,6 +165,8 @@ public class DefaultMavenExecutionRequest
 
     private Map<String, Object> data;
 
+    private boolean useLegacyReactorResolution = false;
+
     public DefaultMavenExecutionRequest()
     {
     }
@@ -209,6 +211,7 @@ public class DefaultMavenExecutionRequest
         copy.setNoSnapshotUpdates( original.isNoSnapshotUpdates() );
         copy.setExecutionListener( original.getExecutionListener() );
         copy.setUseLegacyLocalRepository( original.isUseLegacyLocalRepository() );
+        copy.setUseLegacyReactorResolution( original.isUseLegacyReactorResolution() );
         copy.setBuilderId( original.getBuilderId() );
         return copy;
     }
@@ -1221,6 +1224,17 @@ public class DefaultMavenExecutionRequest
     }
 
     @Override
+    public boolean isUseLegacyReactorResolution()
+    {
+        return this.useLegacyReactorResolution;
+    }
+
+    public MavenExecutionRequest setUseLegacyReactorResolution( boolean value )
+    {
+        this.useLegacyReactorResolution = value;
+        return this;
+    }
+
     public MavenExecutionRequest setBuilderId( String builderId )
     {
         this.builderId = builderId;
